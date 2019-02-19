7252af37075678332601161e18f509245aac0dda
Alex Suarez <alexandprivate@gmail.com>
2019-02-19 17:51:48 -0500

    update

--- src/components/todo/title.js
+++ src/components/todo/title.js
@@ -1,10 +1,14 @@
 import React from "react";
+import version from '../../../package.json'
 const todoIcon = <i className="icon-check"></i>;
 
 const Title = () => (
-  <h1>
-    {todoIcon}<span>to-do</span> kit
-  </h1>
+  <>
+    <h1>
+      {todoIcon}<span>to-do</span> kit
+    </h1>
+    <small>{version.version}</small>
+  </>
 );
 
 export default Title;
