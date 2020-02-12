b45b97e52625f6a0489ccadc7b444e77be04eaa7
Alex <alexandprivate@gmail.com>
2020-02-11 19:28:22 -0500

    update

--- src/components/todo/theme.js
+++ src/components/todo/theme.js
@@ -7,17 +7,10 @@ const iconMoon = <i className="icon-moon"></i>;
 
 const Theme = () => {
   const { theme, changeTheme } = useContext(TodoContext);
-  let styles = {
-    marginTop: 20,
-    height: 36
-  };
+
   return (
     <>
-      <button
-        style={styles}
-        className={`theme-selector ${theme}`}
-        onClick={changeTheme}
-      >
+      <button className={`theme-selector ${theme}`} onClick={changeTheme}>
         {theme === "light" ? <>{iconMoon}</> : <>{iconSun}</>}
       </button>
       <small>{version.version}</small>
